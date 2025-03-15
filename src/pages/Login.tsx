59546d79290c7bf92e92ec03efa9136016d8fa54
quang <quang@DESKTOP-5LDP2E6>
2025-03-15 15:56:35 +0700

    update

--- src/pages/Login.tsx
+++
@@ -1,6 +0,0 @@
-const Login = () => {
-    return <h1>Trang Đăng Nhập</h1>;
-  };
-  
-  export default Login;
-  
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
   ],
-})
+});
